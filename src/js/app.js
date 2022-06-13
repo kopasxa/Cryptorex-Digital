98fff37414e48982411a6bdc15ea90cb2d24dc9b
Pavel <pavelcorolenko535@gmail.com>
2022-06-13 23:04:47 +0300

    add slider

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,29 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
-/*
-import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper = new Swiper();
-*/
+AOS.init();
+
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  allowTouchMove: true,
+  autoplay: {
+    delay: 3000,
+    pauseOnMouseEnter: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+document.querySelectorAll("a").forEach(function (item) {
+    item.onclick = function (e) {
+        e.preventDefault();
+        document.querySelector("#" + e.target.href.split("#")[1]).scrollIntoView({
+            behavior: "smooth",
+        });
+    };
+});
